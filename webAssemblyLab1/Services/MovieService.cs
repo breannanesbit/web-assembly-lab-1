f8208c4d4e607f7157b86655c89cbcfe79ff9ac1
Breanna Nesbit <brebrenesbit02@gmail.com>
2022-11-09 10:27:38 -0700

    both genres for movies and tv shows

--- webAssemblyLab1/Services/MovieService.cs
+++ webAssemblyLab1/Services/MovieService.cs
@@ -13,10 +13,16 @@ namespace webAssemblyLab1.Services
 
         }
 
-        public async Task<IEnumerable<Genre>> GetGenreAsync()
+        public async Task<IEnumerable<Genre>> GetMovieGenreAsync()
         {
-            var arrayOfGenre = await client.GetFromJsonAsync<MovieInfo>("https://api.themoviedb.org/3/movie/550?api_key=989763942fa4f236cb34de985f499dc6");
+            var arrayOfGenre = await client.GetFromJsonAsync<MovieInfo>("https://api.themoviedb.org/3/genre/movie/list?api_key=989763942fa4f236cb34de985f499dc6&language=en-US");
             return arrayOfGenre.genres;
+        }
+
+        public async Task<IEnumerable<Genre>> GetTVShowsGenreAsync()
+        {
+            var arrayOfTvGenre = await client.GetFromJsonAsync<MovieInfo>("https://api.themoviedb.org/3/genre/tv/list?api_key=989763942fa4f236cb34de985f499dc6&language=en-US ");
+            return arrayOfTvGenre.genres;
         }
 
         public async Task<string> GetMovieIdAsync()
