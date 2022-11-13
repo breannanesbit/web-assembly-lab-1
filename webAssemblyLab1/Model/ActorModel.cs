aebbf31af1b83e35459ba17137e6dd32ec1871a5
BobbydpStrange <51657838+BobbydpStrange@users.noreply.github.com>
2022-11-12 20:16:44 -0700

    added more pages and some authorization

---
+++ webAssemblyLab1/Model/ActorModel.cs
@@ -0,0 +1,52 @@
+﻿namespace webAssemblyLab1.Model
+{
+    public class ActorModel
+    {
+
+        public class Cast
+        {
+            public int page { get; set; }
+            public Actor[] results { get; set; }
+            public int total_pages { get; set; }
+            public int total_results { get; set; }
+        }
+
+        public class Actor
+        {
+            public bool adult { get; set; }
+            public int gender { get; set; }
+            public int id { get; set; }
+            public Known_For[] known_for { get; set; }
+            public string known_for_department { get; set; }
+            public string name { get; set; }
+            public float popularity { get; set; }
+            public string profile_path { get; set; }
+        }
+
+        public class Known_For
+        {
+            public bool adult { get; set; }
+            public string backdrop_path { get; set; }
+            public int[] genre_ids { get; set; }
+            public int id { get; set; }
+            public string media_type { get; set; }
+            public string original_language { get; set; }
+            public string original_title { get; set; }
+            public string overview { get; set; }
+            public string poster_path { get; set; }
+            public string release_date { get; set; }
+            public string title { get; set; }
+            public bool video { get; set; }
+            public float vote_average { get; set; }
+            public int vote_count { get; set; }
+            public string first_air_date { get; set; }
+            public string name { get; set; }
+            public string[] origin_country { get; set; }
+            public string original_name { get; set; }
+        }
+
+       
+
+
+    }
+}
--- webAssemblyLab1/Services/MovieService.cs
+++ webAssemblyLab1/Services/MovieService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http.Json;
 using webAssemblyLab1.Model;
+using static webAssemblyLab1.Model.ActorModel;
+using static webAssemblyLab1.Model.DiscoverModel;
 using static webAssemblyLab1.Model.trendingModel;
 
 namespace webAssemblyLab1.Services
@@ -19,6 +21,29 @@ namespace webAssemblyLab1.Services
             //var arrayOfTrendingMovies = new List<MovieInfo>();      
             var arrayOfTrendingMovies = await client.GetFromJsonAsync<Collection>("https://api.themoviedb.org/3/trending/movie/week?api_key=989763942fa4f236cb34de985f499dc6");
             return arrayOfTrendingMovies.results;
+        }
+
+        public async Task<IEnumerable<Result>> GetTrendingTvAsync()
+        {
+            var arrayOfTrendingTv = await client.GetFromJsonAsync<Collection>("https://api.themoviedb.org/3/trending/tv/week?api_key=989763942fa4f236cb34de985f499dc6");
+            return arrayOfTrendingTv.results;
+        }
+
+        public async Task<IEnumerable<Actor>> GetPopularActorAsync()
+        {
+            var arrayOfPopularActors = await client.GetFromJsonAsync<Cast>("https://api.themoviedb.org/3/person/popular?api_key=989763942fa4f236cb34de985f499dc6&language=en-US&page=1");
+            return arrayOfPopularActors.results;
+        }
+        
+        public async Task<IEnumerable<Output>> GetDiscoverAsync()
+        {
+            var arrayOfDiscover = await client.GetFromJsonAsync<Discover>("https://api.themoviedb.org/3/discover/movie?api_key=989763942fa4f236cb34de985f499dc6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate");
+            return arrayOfDiscover.results;
+        }
+       public async Task<IEnumerable<Result>> GetPlayingAsync()
+        {
+            var arrayOfPlaying = await client.GetFromJsonAsync<Collection>("https://api.themoviedb.org/3/movie/now_playing?api_key=989763942fa4f236cb34de985f499dc6&language=en-US&page=1");
+            return arrayOfPlaying.results;
         }
 
         public async Task<IEnumerable<Genre>> GetMovieGenreAsync()
